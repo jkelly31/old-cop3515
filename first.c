acf807b1b0f98339a8a87ebc4c05dae10da6a5f0
jkelly31 <142122577+jkelly31@users.noreply.github.com>
2024-08-30 23:01:12 -0400

    Add files via upload

---
+++ first.c
@@ -0,0 +1,21 @@
+
+
+
+int main()
+{
+    /*printf("\"Hello, World!\"\n");
+    printf("Today's date is 08/28/24\n");
+    
+    */
+/* define vs const */
+   #define SPEED 55
+   printf("The speed limit is %d\n",SPEED);
+
+   /* Column of Numbers Program:
+   print 3x3 square w/ a student name for each of 1-9 students */
+
+    printf("1\t2\t3\n\n");
+    printf("4\t5\t6\n\n");
+    printf("7\t8\t9\n");
+
+}
